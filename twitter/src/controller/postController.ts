8dc51821ce4fb3a2154afc55ad402dee8141c596
singhdikesh <singhdikesh099@gmail.com>
2026-09-14 23:28:54 +0545

    post controller updated

---
+++ twitter/src/controller/postController.ts
@@ -0,0 +1,17 @@
+import type {NextFunction, Request, Response} from "express";
+import { db } from "../prisma/db.ts";
+import { BadRequestException } from "../exception/badRequest.ts";
+import { ErrorCodes } from "../exception/HttpException.ts";
+
+export const getAllPosts = async (req: Request<{id: string}>, res: Response) => {
+    const {id} = req.params;
+
+    const posts = await db.orm.public.Post.where({authorId: id}).all();
+
+    return res.status(200).json({
+        success: true, 
+        data: posts
+    });
+}
+
+export const updatePostById = 
--- twitter/src/controller/userController.ts
+++ twitter/src/controller/userController.ts
@@ -1,7 +1,6 @@
 import type {NextFunction, Request, Response} from "express";
 import bcrypt from "bcrypt";
 import { db } from "../prisma/db.ts";
-import type { NextAction } from "@prisma/orm-postgres/utils";
 import { BadRequestException } from "../exception/badRequest.ts";
 import { ErrorCodes } from "../exception/HttpException.ts";
 
@@ -16,32 +15,85 @@ export const getUserById = async (req: Request<{id: string}>, res: Response, nex
     const user = await db.orm.public.User.where({id}).first();
 
     if(!user){
-        next(new BadRequestException("User not found", ErrorCodes.USER_NOT_FOUND));
+        return next(new BadRequestException("User not found", ErrorCodes.USER_NOT_FOUND));
     }
 
     return res.status(200).json({ success: true, data: user });
 }
 
+export const updateUserById = async (req: Request<{id: string}>, res: Response, next: NextFunction) => {
+    const {id} = req.params;
+    const {name, email, username, password, avatar, coverImage, bio} = req.body;
+
+    const user = await db.orm.public.User.where({id}).first();
+
+    if(!user){
+        return next(new BadRequestException("User not found", ErrorCodes.USER_NOT_FOUND));
+    }
+
+    const updatedFields: Record<string, string> = {};
+    
+    if(name !== undefined) updatedFields.name = name;
+    if(email !== undefined) updatedFields.email = email;
+    if(username !== undefined) updatedFields.username = username;
+    if(password !== undefined) updatedFields.password = await bcrypt.hash(password, 10);
+    if(avatar !== undefined) updatedFields.avatar = avatar;
+    if(coverImage !== undefined) updatedFields.coverImage = coverImage;
+    if(bio !== undefined) updatedFields.bio = bio;
+
+
+    if(Object.keys(updatedFields).length === 0) {
+        return next(new BadRequestException("No fields to update", ErrorCodes.MISSING_REQUIRED_FIELDS));
+    }
+
+    const updatedUser = await db.orm.public.User.where({id}).update(updatedFields);
+
+    return res.status(200).json({
+        success: true, 
+        message: "User updated successfully", 
+        data: updatedUser
+    });
+}
+
+export const deleteUserById = async (req: Request<{id: string}>, res: Response, next: NextFunction) => {
+    const {id} = req.params;
+
+    const user = await db.orm.public.User.where({id}).first();
+
+    if(!user){
+        return next(new BadRequestException("User not found", ErrorCodes.USER_NOT_FOUND));
+    }
+
+    await db.orm.public.User.where({id}).delete();
+
+    return res.status(200).json({
+        success: true, 
+        message: "User deleted successfully"
+    });
+}
+
 export const createUser = async (req: Request, res: Response, next: NextFunction) => {
-        const {name, email, username, password, avatar} = req.body;
+    const {name, email, username, password, avatar, coverImage, bio} = req.body;
 
-        if(!name || !email || !username || !password){
-            next(new BadRequestException("Missing required fields", ErrorCodes.MISSING_REQUIRED_FIELDS));
-        }
+    if(!name || !email || !username || !password){
+        return next(new BadRequestException("Missing required fields", ErrorCodes.MISSING_REQUIRED_FIELDS));
+    }
 
-        const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        const newUser = await db.orm.public.User.create({
-            name, 
-            email, 
-            username, 
-            password: hashedPassword, 
-            avatar
-        });
+    const newUser = await db.orm.public.User.create({
+        username,
+        email,
+        password: hashedPassword,
+        name,
+        bio,
+        avatar,
+        coverImage
+    });
 
-        return res.status(201).json({
-            success: true, 
-            message: "User created successfully", 
-            data: newUser
-        });
+    return res.status(201).json({
+        success: true, 
+        message: "User created successfully", 
+        data: newUser
+    });
 }
